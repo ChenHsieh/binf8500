80854db450985cf3a4ed152c536a6b87052cd2fc
Chen Hsieh <Chen.Hsieh@uga.edu>
2021-10-21 18:19:57 -0400

    stuck at PSSM value

--- PSSM/ch29576PSSM.cpp
+++ PSSM/ch29576PSSM.cpp
@@ -3,245 +3,192 @@
 // ID: ch29576, 811744663
 
 // script for testing
-// g++ -std=c++11 -o ch29576PSSM -Wall ch29576PSSM.cpp; time ./ch29576PSSM FruR.txt FruR.fasta
-// g++ -std=c++11 -o ch29576PSSM -Wall ch29576PSSM.cpp; time ./ch29576PSSM FruR.txt FruR.fasta
+// g++ -std=c++11 -o ch29576PSSM -Wall ch29576PSSM.cpp; time ./ch29576PSSM FruR.txt ecoK12-MG1655.fasta
+// g++ -std=c++11 -O3 -o ch29576PSSM -Wall ch29576PSSM.cpp; time ./ch29576PSSM FruR.txt ecoK12-MG1655.fasta
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
 using namespace std;
-
-// helper functions
-std::string cleanStr(std::string str)
-{
-    // for(unsigned int i = 0; i <= str.length(); i++) // buggy, end of file not handled, switch to this one for sapelo2
-    // {
-    //     if(str[i] == '\n' || str[i] == '\r')
-    //     {
-    //         str.erase(i, 1);
-    //     }
-    // }
-    str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
-    return str;
-}
-// helper function for display format
-int getAlignmentNumber(std::string str)
-{
-    int nonGapNum = 0;
-    int N = str.length();
-    for (unsigned i = 0; i < N; i++)
-    {
-        // cout << str[i];
-        // cout << (str[i] == '-') << endl;
-
-        if (str[i] == '-')
-        {
-            continue;
-        }
-        else
-        {
-            nonGapNum++;
-        }
-    }
-    return nonGapNum;
-}
 
 int main(int argc, char **argv)
 {
     // check input arguments
     cout << endl
-         << "Chen's Needleman-Wunsch global alignment implementation" << endl
+         << "Chen's PSSM implementation" << endl
          << endl;
-    if (argc != 6)
+    if (argc < 3)
     {
         cout << endl
              << endl
-             << endl;
-        cout << "Usage: " << argv[0] << " <input1.fasta> <input2.fasta> <match> <mismatch> <gap>" << endl;
-        cout << "Example: " << argv[0] << " Test01.fasta Test02.fasta 1 0 -1" << endl;
-        cout << endl
+             << endl
+             << "Usage: " << argv[0] << " <AlignmentFile> <SequenceFile> [ScoreCutoff]" << endl
+             << "Example: " << argv[0] << " FruR.txt ecoK12-MG1655.fasta" << endl
+             << endl
              << endl
              << endl;
         exit(1);
     }
 
     // init variables
-    std::ifstream myFile1(argv[1]);
-    std::ifstream myFile2(argv[2]);
-    int match = atoi(argv[3]);
-    int mismatch = atoi(argv[4]);
-    int gap = atoi(argv[5]);
+    std::ifstream alignmentFile(argv[1]);
+    std::ifstream sequenceFile(argv[2]);
+    if (argc > 3)
+    {
+        int scoreCutoff = atoi(argv[3]);
+    }
 
     // read file into a string like a lump sum
     std::stringstream buffer1;
-    buffer1 << myFile1.rdbuf();
+    buffer1 << alignmentFile.rdbuf();
     std::string file_contents1(buffer1.str());
     std::stringstream buffer2;
-    buffer2 << myFile2.rdbuf();
+    buffer2 << sequenceFile.rdbuf();
     std::string file_contents2(buffer2.str());
+    cout << "read file complete" << endl;
 
-    // get the first file content
+    // get gc content
+    std::string header;
+    std::string sequence;
     int start = 0;
-    int end = file_contents1.find("\n", 0);
-    std::string firstLine1 = file_contents1.substr(start, end - start);
-    start = end + 1;
-    std::string content1 = file_contents1.substr(start, file_contents1.size());
-    cout << "File 1: " << argv[1] << endl;
-    cout << "\theader from file 1:\t" << firstLine1 << endl;
-    content1 = cleanStr(content1);
-    cout << "\tcontent from file 1:\t" << content1 << endl;
-    cout << "\tsize of file 1:\t" << content1.size() << endl;
+    int end = file_contents2.find("\n", 0);
+    header = file_contents2.substr(start, end);
+    start= end +1;
+    float whole_length = file_contents2.size();
+    cout << whole_length << endl;
+    sequence = file_contents2.substr(start,whole_length);
+    
+    float gc_count = 0;
+    
+    for (unsigned i = 0; i < whole_length; i++)
+    {
+        if (sequence[i] == 'G' || sequence[i] == 'C')
+        {
+            gc_count++;
+        }
+    }
+    float gc_content = (gc_count / whole_length) / 2;
+    float q[4];
+    q[0] = 0.5 - gc_content;
+    q[1] = gc_content;
+    q[2] = gc_content;
+    q[3] = 0.5 - gc_content;
+
+    // get the alignment file content
+    start = 0;
+    end = file_contents1.find("\n", 0);
+    
+    unsigned lineNum = 0;
+    vector<std::string> seqs;
+    while (end != std::string::npos)
+    {
+        sequence = file_contents1.substr(start, end - start);
+        seqs.push_back(sequence);
+        start = end + 1;
+        end = file_contents1.find("\n", start);
+        lineNum++;
+        // cout << sequence << endl;
+    }
+    cout << lineNum << " sequences in the alignemnt file" << endl
+         << endl;
+
+    const unsigned alignment_length = seqs[0].size();
+    int frequency_matrix[alignment_length][4];
+    float probability_matrix[alignment_length][4];
+    float pssm[alignment_length][4];
+
+    unsigned i, j, k;
+    std::string current_base;
+
+    for (i = 0; i < alignment_length; i++)
+    {
+        // init matrix current row
+        for (j = 0; j < 4; j++)
+        {
+            frequency_matrix[i][j] = 0;
+        }
+        // fill in the count by going through each aligned sequence
+        for (j = 0; j < lineNum; j++)
+        {
+            // current base
+            current_base = toupper(seqs[j][i]);
+            if (current_base == "A")
+            {
+                frequency_matrix[i][0]++;
+            }
+            else if (current_base == "C")
+            {
+                frequency_matrix[i][1]++;
+            }
+            else if (current_base == "G")
+            {
+                frequency_matrix[i][2]++;
+            }
+            else if (current_base == "T")
+            {
+                frequency_matrix[i][3]++;
+            }
+            else
+            {
+                cout << "something's wierd in the alignment file" << endl;
+            }
+        }
+        for (j = 0; j < 4; j++)
+        {
+            probability_matrix[i][j] = (frequency_matrix[i][j] + 0.25) / (lineNum + 1);
+            pssm[i][j] = log(probability_matrix[i][j]);
+        }
+    }
+
+    // printint matrixs
+    cout << "frequency matrix:" << endl
+         << "pos.\tA\tC\tG\tT" << endl;
+    for (i = 0; i < alignment_length; i++)
+    {
+        cout << i + 1 << "\t";
+        for (j = 0; j < 4; j++)
+        {
+            cout << std::fixed << setprecision(3) << frequency_matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "probability matrix:" << endl
+         << "pos.\tA\tC\tG\tT" << endl;
+    for (i = 0; i < alignment_length; i++)
+    {
+        cout << i + 1 << "\t";
+        for (j = 0; j < 4; j++)
+        {
+            cout << std::fixed << setprecision(3) << probability_matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
     cout << endl;
 
-    // get the second file content
-    start = 0;
-    end = file_contents2.find("\n", 0);
-    std::string firstLine2 = file_contents2.substr(start, end - start);
-    start = end + 1;
-    std::string content2 = file_contents2.substr(start, file_contents2.size());
-    cout << "File 2: " << argv[2] << endl;
-    cout << "\theader from file 2:\t" << firstLine2 << endl;
-    content2 = cleanStr(content2);
-    cout << "\tcontent from file 2:\t" << content2 << endl;
-    // print size of content2
-    cout << "\tsize of file 2:\t" << content2.size() << endl
-         << endl
-         << endl;
-
-    cout << "Scores:" << endl
-         << "Match: " << match << ",  Mismatch: " << mismatch << ",  Gap: " << gap << endl
-         << endl;
-
-    // init the matrix
-    cout << "Initializing matrix..." << endl;
-    int row = content2.size() + 1;
-    int col = content1.size() + 1;
-    int *matrix = (int *)malloc((row * col) * sizeof(int));
-    cout << "Initializing matrix...done" << endl
-         << endl;
-    for (unsigned i = 0; i < content1.size() + 1; i++)
+    // still different
+    for (j = 0; j < 4; j++)
     {
-        // cout << "\t\t";
-        // if (i == 0) // fill up the first row
-        // {
-
-        //     for (int j = 0; j < content2.size() + 1; j++)
-        //     {
-        //         cout << content2[j] << "\t";
-        //     }
-        // }
-        // cout << endl;
-        for (unsigned j = 0; j < content2.size() + 1; j++)
-        {
-            if (i == 0) // fill up the first row
-            {
-
-                matrix[j] = j * gap;
-                // if (j == 0)
-                // {
-                //     cout << "\t";
-                // }
-            }
-            else if (j == 0) // fill up the first column
-            {
-                // cout << content1[i - 1] << "\t";
-                matrix[i * row] = i * gap;
-            }
-            else // actual content
-            {
-                // if the character is the same, beware of the offset
-                if (content1[i - 1] == content2[j - 1])
-                {
-                    matrix[i * row + j] = matrix[(i - 1) * row + (j - 1)] + match;
-                }
-                else
-                {
-                    matrix[i * row + j] = std::max(
-                        matrix[(i - 1) * row + j] + gap,
-                        std::max(
-                            matrix[i * row + j - 1] + gap,
-                            matrix[(i - 1) * row + (j - 1)] + mismatch));
-                }
-            }
-            // cout << matrix[i * row + j] << "\t";
-        }
-        // cout << endl;
+        cout << q[j] << endl;
     }
-    cout << "matrix calc done" << endl;
+    cout << "PSSM:" << endl
+         << "pos.\tA\tC\tG\tT" << endl;
+
+    for (i = 0; i < alignment_length; i++)
+    {
+        cout << i + 1 << "\t";
+        for (j = 0; j < 4; j++)
+        {
+
+            cout << std::fixed << setprecision(3) << pssm[i][j] - log(q[j])
+                 << "\t";
+        }
+        cout << endl;
+    }
     cout << endl
-         << "Alignment score: " << matrix[content1.size() * row + content2.size()] << endl
-         << endl;
-    // traverse the matrix to find the alignment
-    bool found = false;
-    int i = content1.size();
-    int j = content2.size();
-    std::string alignment1 = "";
-    std::string alignment2 = "";
-    std::string matching = "";
-    //  backtracing
-    while (!found)
-    {
-        if (matrix[(i)*row + (j)] == matrix[(i - 1) * row + (j)] + gap)
-        {
-            alignment1 = content1[i - 1] + alignment1;
-            alignment2 = "-" + alignment2;
-            matching = " " + matching;
-            i--;
-        }
-        else if (matrix[(i)*row + (j)] == matrix[(i)*row + (j - 1)] + gap)
-        {
-            alignment1 = "-" + alignment1;
-            alignment2 = content2[j - 1] + alignment2;
-            matching = " " + matching;
-            j--;
-        }
-        else if (matrix[(i)*row + (j)] == matrix[(i - 1) * row + (j - 1)] + mismatch)
-        {
-            alignment1 = content1[i - 1] + alignment1;
-            alignment2 = content2[j - 1] + alignment2;
-            matching = " " + matching;
-            i--;
-            j--;
-        }
-        else if (matrix[(i)*row + (j)] == matrix[(i - 1) * row + (j - 1)] + match)
-        {
-            alignment1 = content1[i - 1] + alignment1;
-            alignment2 = content2[j - 1] + alignment2;
-            matching = "*" + matching;
-            i--;
-            j--;
-        }
-        else
-        {
-            // debug
-            cout << i << ":" << j << endl;
-            cout << "Error: " << matrix[(i)*row + (j)] << endl;
-            break;
-        }
-        if (i == 0 && j == 0)
-        {
-            found = true;
-        }
-    }
-
-    // fancy output formatting
-    int alignment_length = alignment1.length();
-    int count1 = 1;
-    int count2 = 1;
-    for (unsigned i = 0; i < alignment_length / 60; i++)
-    {
-        cout
-            << "\t\t" << count1 << "\t" << alignment1.substr(i * 60, 60) << endl;
-        cout << "\t\t"
-             << "\t" << matching.substr(i * 60, 60) << endl;
-        cout << "\t\t" << count2 << "\t" << alignment2.substr(i * 60, 60) << endl
-             << endl;
-        count1 += getAlignmentNumber(alignment1.substr(i * 60, 60));
-        count2 += getAlignmentNumber(alignment2.substr(i * 60, 60));
-    }
-    cout << "\t\t" << count1 << "\t" << alignment1.substr((alignment_length / 60) * 60, alignment_length) << endl;
-    cout << "\t\t"
-         << "\t" << matching.substr((alignment_length / 60) * 60, alignment_length) << endl;
-    cout << "\t\t" << count2 << "\t" << alignment2.substr((alignment_length / 60) * 60, alignment_length) << endl
-         << endl;
+         << "Training set motif scores:" << endl;
 }
